54f283164e974d31575da475a4cc6f02ae87948b
Ryan Hunter <ryanhwilkinson@icloud.com>
2020-04-15 12:09:27 -0500

    update appjs all tests pass...code still not running

--- Develop/app.js
+++ Develop/app.js
@@ -10,118 +10,111 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 ​
 const render = require("./lib/htmlRenderer");
 ​
-​
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-async function prompt() {
-    let responseDone = "";
-    // prompt to collect input and use do while at least once and do it number of times depending on the while condition
-    do {
-         try {
-              response = await inquirer.prompt([
+​const teamMembers = [];
+const idArray = [];
 
-                   {
-                        type: "input",
-                        name: "name",
-                        message: "What is the employee's name?: "
-                   },
-                   {
-                        type: "input",
-                        name: "id",
-                        message: "Please enter the employee's ID: "
-                   },
-                   {
-                        type: "input",
-                        name: "email",
-                        message: "Enter the employee's email address: "
-                   },
-                   {
-                        type: "list",
-                        name: "role",
-                        message: "Please choose the employee's role:",
-                        choices: [
-                             "Engineer",
-                             "Intern",
-                             "Manager"
-                        ]
-                   }
-              ]);
+const getQuestions = teamMember => [
+  {
+    type: 'input',
+    name: 'name',
+    message: `What is your ${teamMember}'s name?`
+  },
+  {
+    type: 'input',
+    name: 'id',
+    message: `What is your ${teamMember}'s id?`
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: `What is your ${teamMember}'s email?`
+  }
+];
 
-              let response2 = ""
-              // if else statement
+const teamMenu = () => {
+  const createManager = async () => {
+    const response = await inquirer.prompt([
+      ...getQuestions('manager'),
+      {
+        type: 'input',
+        name: 'officeNumber',
+        message: "what is your manager's office number?"
+      }
+    ]);
 
-              if (response.role === "Engineer") {
-                   response2 = await inquirer.prompt([{
-                        type: "input",
-                        name: "x",
-                        message: "What is the employee's github username?:",
-                   }, ]);
-                   //store the object and push
-                   const engineer = new Engineer(response.name, response.id, response.email, response2.x);
-                   teamArray.push(engineer);
+    const { name, id, email, officeNumber } = response;
+    const manager = new Manager(name, id, email, officeNumber);
 
-              } else if (response.role === "Intern") {
-                   response2 = await inquirer.prompt([{
-                        type: "input",
+    teamMembers.push(manager);
+    idArray.push(id);
 
-                        //the x is to only store into the team array
-                        name: "x",
-                        message: "What school is the employee attending?:",
-                   }, ]);
-                   //store the object and push
-                   const intern = new Intern(response.name, response.id, response.email, response2.x);
-                   teamArray.push(intern);
+    createTeam();
+  };
 
-              } else if (response.role === "Manager") {
-                   response2 = await inquirer.prompt([{
-                        type: "input",
-                        name: "x",
-                        message: "What is the employee's office number?:",
-                   }, ]);
-                   //store the object and push
-                   const manager = new Manager(response.name, response.id, response.email, response2.x);
-                   teamArray.push(manager);
-              }
-         } catch (err) {
-              return console.log(err);
-         }
-         console.log(teamArray)
-         //need to prompt do you want to continue
+  const addEngineer = async () => {
+    const response = await inquirer.prompt([
+      ...getQuestions('engineer'),
+      {
+        type: 'input',
+        name: 'github',
+        message: "What is your engineer's GitHub username?"
+      }
+    ]);
 
-         responseDone = await inquirer.prompt([{
-              type: "list",
-              name: "finish",
-              message: "Do you want to continue?: ",
-              choices: [
-                   "Yes",
-                   "No"
-              ]
-         }, ]);
+    const { name, id, email, github } = response;
+    const engineer = new Engineer(name, id, email, github);
 
-         // console.log(responseDone.choices);
-         //the while parameter is saying continue running the code if the user selects "yes"
-    } while (responseDone.finish === "Yes");
-}
+    teamMembers.push(engineer);
+    idArray.push(id);
 
-prompt();
+    createTeam();
+  };
 
-​
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-​
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-​
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-​
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an 
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
+  const addIntern = async () => {
+    const response = await inquirer.prompt([
+      ...getQuestions('intern'),
+      {
+        type: 'input',
+        name: 'school',
+        message: "What is your intern's school?"
+      }
+    ]);
+
+    const { name, id, email, school } = response;
+    const intern = new Intern(name, id, email, school);
+
+    teamMembers.push(intern);
+    idArray.push(id);
+
+    createTeam();
+  };
+
+  const createTeam = async () => {
+    const response = await inquirer.prompt([
+      {
+        type: 'list',
+        name: 'memberChoice',
+        message: 'Which type of team member would you like to add?',
+        choices: ['Engineer', 'Intern', "I don't want to add any more team members"]
+      }
+    ]);
+
+    switch (response.memberChoice) {
+      case 'Engineer':
+        addEngineer();
+        break;
+
+      case 'Intern':
+        addIntern();
+        break;
+
+      default:
+        fs.writeFileSync(outputPath, render(teamMembers), 'utf-8');
+        break;
+    }
+  };
+
+  createManager();
+};
+
+teamMenu();
